464346c97f4721ddaec8be6b923e86e0aba49c1e
Matheus <matheusdsouzabuth@gmail.com>
2025-04-16 10:29:42 -0300

    Starting project

---
+++ src/test/java/com/buthdev/sistematizacao/SistematizacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buthdev.sistematizacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistematizacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
